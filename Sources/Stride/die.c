7e9f3924488b126837b75590e02d3b260e167e4a
Jorgealfue10 <jorgealonsodelafuente@gmail.com>
2022-08-30 16:00:02 +0200

    Goodbye die

--- Sources/Stride/die.c
+++ Sources/Stride/die.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 void die(char *format, ... ) {
-printf("\nError in Stride\n");
+//printf("\nError in Stride\n");
 void exit(int return_code);
 va_list ptr;
 va_start(ptr,format);
-vfprintf(stderr,format,ptr);
+//vfprintf(stderr,format,ptr);
 //exit(1);
 va_end(ptr);
 }
